2f3f671d1e49b91575d4f490f55af49d9f7bf687
ebjtvlf <54845469+ebjtvlf@users.noreply.github.com>
2019-10-31 00:41:51 +0900

    Update lazyCounter.js

--- lazyCounter.js
+++ lazyCounter.js
@@ -1,37 +1,35 @@
+/*
+lazyCounter.js
+Copyright (c) 2019 Yusaku Ichimura
+This is released under the MIT License.
+*/
 (function(){  
-  $.fn.lazyCounter = function(countNow, countNew) {
-      var countUp = function() {
-          var counter = 0;
-          var same    = 0;
-          var abs     = 0;
-          dulation    = 1000;
-          var len =  (countNew + "").length;
-          for(var i = 0; i < len; i++) {
-
-              var abc = (countNew % (10 ** (i + 1)))/(10 ** i);
-              var def = Math.floor(abc);
-
-              var abc2 = (countNow % (10 ** (i + 1)))/(10 ** i);
-              var def2 = Math.floor(abc2);
-
-              var plusMinus = 1;
-              abs += Math.abs(def - def2);
-              if((def - def2) < 0) {
-                  plusMinus = -1;
-              } else if((def - def2) === 0) {
-                  plusMinus = 0;
-                  same++;
-              }
-              counter += (def2 + plusMinus) * (10 ** i);
-          }
-          countNow = counter;
-          this.text(countNow);
-          if(countNew === countNow) {
-              return;
-          }
-          dulation = dulation/(1 + len - same + abs);
-          setTimeout($.lazyCounter(countNow, countNew), dulation);
-      };
-      countUp();
-      }
-  })(jQuery);
+    $.fn.lazyCounter = function(countNow, countNew, dulation) {
+        var counter = 0;
+        var same    = 0;
+        var abs     = 0;
+        var _dulation= dulation || 1000;
+        for(var i = 0, len = (countNew + "").length; i < len; i++) {
+            var abc  = (countNew % (10 ** (i + 1)))/(10 ** i);
+            var def  = Math.floor(abc);
+            var abc2 = (countNow % (10 ** (i + 1)))/(10 ** i);
+            var def2 = Math.floor(abc2);
+            var plusMinus = 1;
+            abs += Math.abs(def - def2);
+            if((def - def2) < 0) {
+                plusMinus = -1;
+            } else if((def - def2) === 0) {
+                plusMinus = 0;
+                same++;
+            }
+            counter += (def2 + plusMinus) * (10 ** i);
+        }
+        countNow = counter;
+        this.text(countNow);
+        if(countNew === countNow) {
+            return;
+        }
+        dulation = dulation/(1 + len - same + abs);
+        setTimeout($.lazyCounter(countNow, countNew, _dulation), _dulation);
+    };
+})(jQuery);
